3ad1ee63dc9f5106520e2a79c7370493785c3a22
Seunghwan <seunghwansohn@gmail.com>
2019-10-15 10:15:39 +0700

    기본틀 구축 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import TopTitle from "./components/TopTitle"
+import TopBar from "./components/TopBar";
+import SubMenu from "./components/SubMenu";
+import ContentWindow from "./components/ContentWindow"
+import TOC from "./components/TOC"
 import './App.css';
 
 class App extends Component { 
@@ -7,22 +10,49 @@ class App extends Component {
     super(props);               //2-2. State를 선언하는 일반 구문
     this.state = {              //2-3. State를 선언하는 일반 구문
       mode:'read',              //현재 페이지의 기본성격을 mode라는 state로 변수화
+      selected_content_id:2,
       toptitle:'Sohntech Management System',
-      subject:{title:'WEB', sub:'World Wid Web!'},
-      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
-        {id:1, title:'HTML', desc:'HTML is for information'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+        {id:0, title:'Top Page', desc:'Menus'},
+        {id:1, title:'Search Item', desc:'Search Product'},
+        {id:2, title:'Projects', desc:'Search Product'}
       ]
     }
   }
+
   render(){
-  console.log('됐음');
-  return (
-    <div className="App">
-     <TopTitle title = {this.state.toptitle}/>
-    </div>
+    var _title, _desc = null;
+    if(this.state.mode ===  'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
+    return (
+      <div className="App">
+      <TopBar title = {this.state.toptitle}/>
+      <TOC 
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+          }.bind(this)} 
+          data={this.state.contents}
+      ></TOC>
+      <ContentWindow title={_title} desc={_desc}></ContentWindow>
+      
+      
+      </div>
   );
 }
 }
